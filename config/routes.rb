ee13131c14d142b982fbbcaac9e33d2075dba5de
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-07 16:56:08 +0300

    Include entites into routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :categories do
+    resources :entities, only: [:new, :create]
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
