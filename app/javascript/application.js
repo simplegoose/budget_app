6bdf9740d4e1b641652a854ea011cf720bfa671a
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-08 22:18:10 +0300

    Import jquery to application Create a function that uses jquery utils to toggle navigation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'jquery'
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min
+// require turbolinks
+//= require_tree .
 
 const hideNotice = () => {
   const notices = document.querySelectorAll('.alert');
@@ -9,6 +16,18 @@ const hideNotice = () => {
     if (notice) notice.classList.add('d-none')
   })
 }
+
+const toggleMenu = () => {
+  if ($('.navigation').hasClass('show')) {
+    $('.navigation').animate({ left: '-75vw' }).removeClass('show');
+    $('body').animate({ left: '0' }).removeClass('hide');
+  } else {
+    $('.navigation').animate({ left: '0' }).addClass('show');
+    $('body').animate({ left: '75vw' }).addClass('hide');
+  }
+}
 document.addEventListener("turbo:load", () => {
   setInterval(hideNotice, 5000);
+
+  $('.menu-button').on('click', toggleMenu);
 });
